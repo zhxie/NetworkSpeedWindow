d17ce77cdcfa1610d8d4c13ee52d9bd86d6e20d1
Xie Zhihao <xzh1206@gmail.com>
2024-11-17 21:27:07 +0800

    Initial Commit

---
+++ SpeedFloat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeedFloat
+//
+//  Created by 谢之皓 on 2024/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeedFloat/SpeedFloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedFloatApp.swift
+//  SpeedFloat
+//
+//  Created by 谢之皓 on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct SpeedFloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
